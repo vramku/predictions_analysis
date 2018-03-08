40e1e6da93422368c832406d3b7be7a58eddf92c
andreygal <andreygalper@gmail.com>
2018-03-08 05:25:51 -0500

    fixed bash script

--- gtfs/createstops.sh
+++ gtfs/createstops.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 #concatenates stop id file and creates a scv with stop_id and stop_name columns
-outfile=$1
-(head -1 stopgtfsbx.csv ; tail -n +2 -q stopgtfs*.csv) | awk -F, '{print $1","$2}' > "$outfile"
+echo "Name of an output file?"
+read OUTFILE 
+(head -1 stopgtfsbx.csv ; tail -n +2 -q stopgtfs*.csv) | \
+awk -F, '{print $1","$2}' | \
+sort | \
+awk -F, '{ if (a[$1]++ == 0) print $0; }' "$@" \
+> $OUTFILE
 
