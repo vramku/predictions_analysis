1e72d8f032cb63cc4ad41e328191a37c06bb437e
andreygal <andreygalper@gmail.com>
2018-03-08 05:27:57 -0500

    bash

--- gtfs/createstops.sh
+++ gtfs/createstops.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #concatenates stop id file and creates a scv with stop_id and stop_name columns
-echo "Name of an output file?"
+echo "Name for an output file?"
 read OUTFILE 
 (head -1 stopgtfsbx.csv ; tail -n +2 -q stopgtfs*.csv) | \
 awk -F, '{print $1","$2}' | \
