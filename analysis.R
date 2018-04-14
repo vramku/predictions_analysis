3bff228790ea54290db8d7911cd2a15b765ef893
andreygal <andreygalper@gmail.com>
2018-04-13 20:09:04 -0400

    working on graphing results

--- analysis.R
+++ analysis.R
@@ -6,6 +6,8 @@ library(RSQLite)
 library(tidyr)
 library(lubridate)  #epoch to posix_ct conversion
 library(doParallel) #multicore processing for the plyr::ddply step
+library(data.table)
+library(ggplot2)
 
 #set the timezone using IANA convention 
 timezone <- "America/New_York"
@@ -16,18 +18,18 @@ org_coef = c(0.4, 0.4, 0.2)
 
 drv <- dbDriver("SQLite")
 con <- dbConnect(drv, 'test.db', flags = SQLITE_RO)
-Pred_Data_Complt <- dbGetQuery(con, "SELECT vehicle, time_stamp, stop_gtfs_seq, hist_cum, rece_cum, sche_cum,
-                                            predicted_t, measured_t
+Pred_Data_Complt <- dbGetQuery(con, "SELECT vehicle, t_stamp, stop_gtfs_seq, hist_cum, rece_cum, sche_cum,
+                                            t_predicted, t_measured
                                      FROM mta_bus_data")
 dbDisconnect(con)
-Pred_Data_Complt <- transform(Pred_Data_Complt, time_stamp = as_datetime(as.double(time_stamp), tz = timezone))
+Pred_Data_Complt <- transform(Pred_Data_Complt, t_stamp = as_datetime(as.double(t_stamp), tz = timezone))
 
 #bin and residual cutoffs in seconds 
 bin_cutoffs <- c(0, 120, 240, 360, 600, 900, 1200, Inf)
 res_cutoffs <- c(0, 60, 120, 240, 360, Inf)
 
 #use biglm if you run out of memory; lmtest library to test the model 
-Pred_Data_Complt$pred_bin <- cut(Pred_Data_Complt$predicted_t, bin_cutoffs,  dig.lab = 5 )
+Pred_Data_Complt$pred_bin <- cut(Pred_Data_Complt$t_predicted, bin_cutoffs,  dig.lab = 5 )
 
 #calculate linear models for every subgroup 
 bin_lvl <- levels(Pred_Data_Complt$pred_bin)
@@ -36,6 +38,8 @@ bin_names <- paste0((bin_cutoffs[-length(bin_cutoffs)]) / 60, " to ", (bin_cutof
 res_names <- c(paste0((res_cutoffs[-length(res_cutoffs)]) / 60,  " to ", (res_cutoffs[-1]) / 60, " mins"), "Total")
 metric_names <- c("Bin_Str", "Coef_Matrix", "Optim_Model", "Metric_Matrix", "AbsRes_Matrix")
 mod_names <- c("Original", "Optimized", "Normalized")
+coef_names <- c("Historical", "Recent", "Schedule")
+meas_names <- c("R2 (Pearson)", "SD", "Mean", "Median")
 
 bin_metrics <- vector("list", length(bin_lvl))
 bin_metrics <- setNames(bin_metrics, bin_lvl)
@@ -51,15 +55,15 @@ for (bin in seq_along(bin_metrics)) {
   bin_metrics[[bin]] <- setNames(bin_metrics[[bin]], metric_names)
   bin_metrics[[bin]][["Bin_Str"]] <- bin_names[bin]
   bin_metrics[[bin]][["Optim_Model"]] <- vector("list", length = 12)
-  bin_metrics[[bin]][["Coef_Matrix"]] <- matrix(nrow = 3, ncol = 3, dimnames = (list(mod_names, c("Historical", "Recent", "Schedule"))))
+  bin_metrics[[bin]][["Coef_Matrix"]] <- matrix(nrow = 3, ncol = 3, dimnames = (list(mod_names, coef_names)))
   bin_metrics[[bin]][["Coef_Matrix"]]["Original",] <- org_coef
-  bin_metrics[[bin]][["Metric_Matrix"]] <- matrix(nrow = 3, ncol = 4, dimnames = (list(mod_names, c("R2 (Pearson)", "SD", "Mean", "Median"))))
+  bin_metrics[[bin]][["Metric_Matrix"]] <- matrix(nrow = 3, ncol = 4, dimnames = (list(mod_names, meas_names)))
   bin_metrics[[bin]][["AbsRes_Matrix"]] <- matrix(nrow = 3, ncol = length(res_names), dimnames = list(mod_names, res_names))
 }
 
 calc_model <- function(bin_df) {
   bin_inx <- match(bin_df$pred_bin[1], names(bin_metrics))
-  mod_formula <- formula(bin_df$measured_t ~ bin_df$hist_cum + bin_df$rece_cum + bin_df$sche_cum + 0)
+  mod_formula <- formula(bin_df$t_measured ~ bin_df$hist_cum + bin_df$rece_cum + bin_df$sche_cum + 0)
   optimized_model <- lm(mod_formula, bin_df)
   bin_metrics[[bin_inx]][["Optim_Model"]] <<- optimized_model
   optim_coef <- coefficients(optimized_model)
@@ -78,24 +82,24 @@ calc_new_pred <- function(bin_df) {
   calc_new_t <- function(h, r, s, type) {
     return(h * coef_matrix[type, "Historical"] + r * coef_matrix[type, "Recent"] + s * coef_matrix[type, "Schedule"])
   }
-  bin_df <- mutate(bin_df, a_res_orig = abs(bin_df$measured_t - bin_df$predicted_t),
+  bin_df <- mutate(bin_df, a_res_orig = abs(bin_df$t_measured - bin_df$t_predicted),
                            pred_t_optim = calc_new_t(bin_df$hist_cum, bin_df$rece_cum, bin_df$sche_cum, "Optimized"))
-  bin_df <- mutate(bin_df, a_res_optim = abs(bin_df$measured_t - bin_df$pred_t_optim),
+  bin_df <- mutate(bin_df, a_res_optim = abs(bin_df$t_measured - bin_df$pred_t_optim),
                            pred_t_norm = calc_new_t(bin_df$hist_cum, bin_df$rece_cum, bin_df$sche_cum, "Normalized"))
-  bin_df <- mutate(bin_df, a_res_norm = abs(bin_df$measured_t - bin_df$pred_t_norm))
+  bin_df <- mutate(bin_df, a_res_norm = abs(bin_df$t_measured - bin_df$pred_t_norm))
 }
   
 doParallel::registerDoParallel(detectCores() - 1)
-Pred_Data_Analyzed <- plyr::ddply(Pred_Data_Complt, .(pred_bin), calc_new_pred, .parallel = T)
+Pred_Data_Analyzed <- as.data.table(plyr::ddply(Pred_Data_Complt, .(pred_bin), calc_new_pred, .parallel = T))
 
 res_funs <- list(sd = function(x) sd(x), mean = function(x) mean(x), median = function(x) median(x))
 calc_metr <- function(bin_df) {
-  rows <- c("predicted_t", "pred_t_optim", "pred_t_norm")
+  rows <- c("t_predicted", "pred_t_optim", "pred_t_norm")
   rsds <- c("a_res_orig", "a_res_optim", "a_res_norm")
   dim_names = (list(mod_names, c("R2 (Pearson)", "SD", "Mean", "Median")))
   metr_mat <- matrix(nrow = 3, ncol = 4, dimnames = dim_names)
   for (row in seq_along(rows)) { 
-    metr_mat[row, 1] <- cor(bin_df$measured_t, bin_df[rows[row]])^2 
+    metr_mat[row, 1] <- cor(bin_df$t_measured, bin_df[rows[row]])^2 
     for (cell in seq_along(metr_mat[row, 2:4])) {
       metr_mat[row, (cell + 1)] <- res_funs[[cell]](bin_df[[rsds[row]]])
     }
@@ -129,4 +133,24 @@ for (bin in seq_along(bins)) {
   bin_metrics[[bin]][["AbsRes_Matrix"]][1:3,] <- count_residuals(bins[[bin]])
 }
 
-  
+summary_matrix <- matrix(ncol = 14, nrow = length(bin_metrics) * 3, dimnames = list(rep(mod_names, times = length(bin_metrics)), 
+                                                                                   c(res_names, meas_names, coef_names, "Bin")))
+metric_arr <- c("AbsRes_Matrix", "Metric_Matrix", "Coef_Matrix")
+for (bin in seq_along(bin_metrics)) {
+  mats <- sapply(str_c("bin_metrics[[", bin, "]]$", metric_arr), function(x) eval(parse(text = x)))
+  mats <- c(mats, list((matrix(rep(names(bin_metrics[bin]), times = 3)))))
+  summary_matrix[(1 + (bin - 1) * 3):(bin * 3),] <- Reduce(cbind, mats)
+}
+
+#convert the summary matrix to a data.table 
+dts <- as.data.table(summary_matrix, keep.rownames = T)
+dts <- dts[, lapply(.SD, function(x) round(as.numeric(x), 4)), by = .(Bin, rn)]
+
+#Prepare data for faceted plotting using ggplot2
+Pred_Data_ResMolt <- melt(Pred_Data_Analyzed, id.vars = c(1:9,11,13))
+Pred_Data_ResMolt <- Pred_Data_ResMolt %>% mutate(facet = ifelse(.$variable == "a_res_orig", "Original", 
+                                                                  ifelse(.$variable == "a_res_optim", "Optimized",
+                                                                         "Normalized")))
+
+Pred_Data_ResMolt %>% ggplot(.) + 
+  geom_bar(mapping = aes(x = pred_bin, fill = cut(.$value, res_cutoffs)), position = "dodge") + facet_grid(facet~.)
