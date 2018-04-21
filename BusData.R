da77d69a5e291aa46c0cdffa09f6f54d132fafb7
andreygal <andreygalper@gmail.com>
2018-04-21 01:28:15 -0400

    building R6 BusData

--- BusData.R
+++ BusData.R
@@ -1,4 +1,5 @@
 library(R6)
+library(RSQLite)
 
 BusData <- R6Class(
   # Set the name for the class; figure out how to chain method calls for in place data manipulation 
@@ -8,34 +9,55 @@ BusData <- R6Class(
     db_con     = "S4",
     route      = "character",
     is_express = "integer",
-    pred_data  = "list",
+    pred_data  = NULL,
     #SQLite Query Params
     q_fields = c("vehicle", "t_stamp", "stop_gtfs_seq", "hist_cum", "rece_cum", "sche_cum", 
-                 "t_predicted", "t_measured", "route, depot", "is_express")
+                 "t_predicted", "t_measured", "route", "depot", "is_express"),
+    q_table  = "mta_bus_data"
     ),
   public = list(
    print = function() {
-      cat("BusData Elements:\n")
+      cat("BusData Elements:\n", private$is_express, private$route, "\n")
       print(private$db_con)
     },
-    initialize = function(db_con, is_express = NULL, route = NULL) {
+    get_pred_data = function(db_con, is_express) {
+     query <- paste0("SELECT ", str_c(private$q_fields, collapse = ', '), " FROM ", private$q_table)
+     if (is.null(is_express)) {
+       private$pred_data <- dbGetQuery(private$db_con, query)
+     } else {
+       private$pred_data <- dbGetQuery(db_con, paste0(query, " WHERE is_express = ", is_express))
+     }
+     private$pred_data <- transform(private$pred_data, t_stamp = as_datetime(as.double(t_stamp), tz = "America/New_York"))
+   }, 
+   initialize = function(db_con, is_express = NULL, route = NULL) {
       private$db_con <- db_con
       private$is_express <- is_express 
-      private$route  <- route 
+      private$route  <- route
+      private$pred_data <- self$get_pred_data(db_con, is_express)
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
       },
     get_q_fields = function() {
       print(private$q_fields)
     },
-    get_pred_data <- function(con, is_express) {
-     query <- "SELECT vehicle, t_stamp, stop_gtfs_seq, hist_cum, rece_cum, sche_cum, 
-                   t_predicted, t_measured, route, depot, is_express
-               FROM mta_bus_data"
-     if (missing(is_express)) {
-       return(dbGetQuery(con, query))
-     } else {
-       return(dbGetQuery(con, paste0(query, " WHERE is_express = ", is_express)))
-     }
-   } 
+    get_int_data = function() {
+      head(private$pred_data, 100)
+    }
+    
    
    )
 )
