17b1e7a24d892fcd9fa4d490aec88579092bfc54
andreygal <andreygalper@gmail.com>
2018-07-18 21:51:41 -0400

    implemented drip filter prototype

--- drip_analysis.R
+++ drip_analysis.R
@@ -16,9 +16,32 @@ dbDisconnect(con)
 data <- transform(data, t_stamp = as_datetime(as.double(t_stamp)))
 #drip_track <- data_5388[block == unique(drip_sample$block),][date(t_stamp) == "2017-11-11" & shape == "MTA_Q100466" & hour(t_stamp) %in% c(6,7)]
 
+#pass a cut based on vehicle and block
 drip_cut <- function(df) {
-  rows_in_df <- nrow(df)
+  max_gtfs <- max(drip_samp$stop_gtfs_seq)
+  stop_slices <- vector("list", length = max_gtfs)
   
+  for (stop in 1:max_gtfs) {
+    print(stop_slices[[stop]] <- filter(df, stop_gtfs_seq == stop))
+  }
+  
+  print(stop_slices)
+  drip_stamp <- vector("list")
+  rows_in_gtfs_one <- nrow(stop_slices[[1]])
+  if(rows_in_gtfs_one < 4) return()
+  lag_inx <- 1 
+  lead_inx <- 4 
+  while (!(lead_inx == rows_in_gtfs_one)) {
+    #bus has not moved more than 30m for the last 4 signals
+    dist_advanced <- stop_slices[[1]]$dist_covered[lead_inx] - stop_slices[[1]]$dist_covered[lag_inx]
+    print(dist_advanced)
+    if (dist_advanced < 30) {
+      append(drip_stamp, stop_slices[[1]]$t_stamp[lag_inx])
+      print(stop_slices[[1]]$t_stamp[lag_inx])
+    }
+    lead_inx <- lead_inx + 1
+    lag_inx <- lag_inx + 1
+  }
 }
 
 
